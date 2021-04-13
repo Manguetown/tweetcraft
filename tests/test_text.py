903394becce4813dc5738050289a8e80f7c1a5fb
João Ribeiro Medeiros <joao@MacBook-Air-de-Joao.local>
2021-04-13 19:31:54 -0300

    corrected syntax mistake

--- tests/test_text.py
+++ tests/test_text.py
@@ -11,9 +11,7 @@ class TestTextProcessing(unittest.TestCase):
 
     def test_remove_url(self):
         got = remove_url(['oh well whatever nevermind https://t.co/asd', 'come as you are www.nirvana.se'])
-        expected = [['oh well whatever nevermind  ',  
-                    ['come as you are  '],
-                    ]
+        expected = ['oh well whatever nevermind  ',  'come as you are  ']
         self.assertEqual(got, expected)
 
     def test_tokenize_text(self):
