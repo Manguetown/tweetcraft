5a3ae1afde141007f197c4c9f2fa5468e3e586b1
João Ribeiro Medeiros <joao@MacBook-Air-de-Joao.local>
2021-04-27 19:27:50 -0300

    simplifica ScrapeTwint.py, tendo resolvido a instalacao nos requirements

--- scrape/ScrapeTwint.py
+++ scrape/ScrapeTwint.py
@@ -4,17 +4,6 @@
 import twint
 import os
 
-class ScrapeTwint:
-
-def __init__(self):
-    pass
-
-def InstallTwint(self):
-    os.system('git clone --depth=1 https://github.com/twintproject/twint.git')
-    os.system('cd twint')
-    os.system('pip3 install . -r requirements.txt')
-    pass
-
 def ScrapeHashtagTwint(hashtag, datetime):
     os.system('twint -s '+ hashtag +' --since "' + datetime +'"  -o '+ hashtag +'tweets.csv --csv ')
     pass
