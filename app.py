b2c5c61bab9db1b5232e5480ccde2b5ef2020d04
João Ribeiro Medeiros <joao@MacBook-Air-de-Joao.local>
2021-04-29 19:17:51 -0300

    corrige ambiguidade em app.py

--- app.py
+++ app.py
@@ -51,14 +51,14 @@ ScrapeHashtagTwint(hash, horadia)
 
 if hash:
     text = ProcessText(hash)
-    remove_columns = ['id', 'conversation_id', 'created_at', 'date', 'time', 'timezone',
+    ColumnsToRemove = ['id', 'conversation_id', 'created_at', 'date', 'time', 'timezone',
             'user_id', 'username', 'name', 'place',  'language', 'mentions',
             'urls', 'photos', 'replies_count', 'retweets_count', 'likes_count',
             'hashtags', 'cashtags', 'link', 'retweet', 'quote_url', 'video',
             'thumbnail', 'near', 'geo', 'source', 'user_rt_id', 'user_rt',
             'retweet_id', 'reply_to', 'retweet_date', 'translate', 'trans_src',
             'trans_dest']
-    text = text.remove_columns(remove_columns).select_tweets().remove_url().tokenize()
+    text = text.remove_columns(ColumnsToRemove).select_tweets().remove_url().tokenize()
 
     WC = TextProcess.get_text_cloud(text.tokenized_text)
 
