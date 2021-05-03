8c690922f795078ff50943d2309060fbc2d2a542
nahumsa <nahumsaa@gmail.com>
2021-05-03 11:06:41 -0300

    Add coverage

--- noxfile.py
+++ noxfile.py
@@ -3,17 +3,24 @@ import nox
 import os
 
 @nox.session
-def tests(session):
+def tests_coverage(session):
+    """ Install all requirements, run pytest.
+    """
     session.install("-r", "requirements.txt")
-    session.run("pytest")
+    session.run("coverage", "run",  "-m",  "--omit=.nox/*", "pytest")
+    session.run("coverage", "report", "--fail-under=70", "--show-missing")
+    session.run("coverage", "erase")
+    
 
 @nox.session
 def lint(session):
+    """ Lint using flake8.
+    """
     session.install("flake8")
     
     folders = ["text", "tests"]
     
     for folder_name in folders:
         for files in os.listdir(folder_name):
-            if files[0] != '_':
+            if files[0] not in ['_', '.']:
                 session.run("flake8", f"{folder_name}/{files}")
