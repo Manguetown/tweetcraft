b381ba3daa87aea9ceb764559455878b454122f0
João Ribeiro Medeiros <joao@MacBook-Air-de-Joao.local>
2021-05-04 04:59:46 -0300

    corrige erros em app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from text.text import TextProcess
 from scrape.ScrapeTwint import ScrapeHashtagTwint
 from datetime import datetime, timedelta
 
-class ProcessText:
+class WriteApp:
     def __init__(self, path):
         self._df = pd.read_csv(path + 'tweets.csv', sep='\t')
         self.tweets = None
@@ -39,13 +39,14 @@ class ProcessText:
         self.tweets_tokenized = TextProcess.tokenize_text(self.tweets)
         return self
     
-    def generate_wordcloud(max_font_size: int, width: int, height: int, figsize: tuple):
+    def generate_wordcloud(self, max_font_size: int, width: int, height: int, figsize: tuple):
+        WC = TextProcess.get_text_cloud(self.tweets_tokenized)
         word_cloud = WordCloud(max_font_size = max_font_size, width = width, height = height)
         word_cloud.generate(WC)
         imagem = plt.figure(figsize = figsize)
         plt.imshow(word_cloud)
         plt.axis('off')
-
+        plt.show()
 
 
 
@@ -59,20 +60,16 @@ hash = st.text_input('Scrape Twitter for your target Hashtag! ;)')
 ScrapeHashtagTwint(hash, horadia)
 
 if hash:
-    text = ProcessText(hash)
+    text = WriteApp(hash)
     columns_to_temove = ['id', 'conversation_id', 'created_at', 'date', 'time', 'timezone',
-                'user_id', 'username', 'name', 'place',  'language', 'mentions',
-                'urls', 'photos', 'replies_count', 'retweets_count', 'likes_count',
-                'hashtags', 'cashtags', 'link', 'retweet', 'quote_url', 'video',
-                'thumbnail', 'near', 'geo', 'source', 'user_rt_id', 'user_rt',
-                'retweet_id', 'reply_to', 'retweet_date', 'translate', 'trans_src',
-                'trans_dest']
-    text = text.remove_columns(columns_to_remove).select_tweets().remove_url().tokenize()
+                         'user_id', 'username', 'name', 'place',  'language', 'mentions',
+                         'urls', 'photos', 'replies_count', 'retweets_count', 'likes_count',
+                         'hashtags', 'cashtags', 'link', 'retweet', 'quote_url', 'video',
+                         'thumbnail', 'near', 'geo', 'source', 'user_rt_id', 'user_rt',
+                         'retweet_id', 'reply_to', 'retweet_date', 'translate', 'trans_src',
+                         'trans_dest']
+    text = text.remove_columns(columns_to_temove).select_tweets().remove_url().tokenize()
 
-    WC = TextProcess.get_text_cloud(text.tweets_tokenized)
-
-    word_cloud = WordCloud(max_font_size = 500, width = 500, height = 535)
-
-    st.write(generate_wordcloud(500, 500, 535, (16, 9)))
+    st.write(text.generate_wordcloud(500, 500, 535, (16, 9)))
 
 st.button("Re-run")
