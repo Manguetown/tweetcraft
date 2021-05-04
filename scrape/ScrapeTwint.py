b466e3d8a55b8826506b210a974ae0ab77c7bedc
João Ribeiro Medeiros <joao_rmedeiros@yahoo.com.br>
2021-05-04 15:04:07 -0300

    Update scrape/ScrapeTwint.py
    Co-authored-by: Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>

--- scrape/ScrapeTwint.py
+++ scrape/ScrapeTwint.py
@@ -1,8 +1,6 @@
-import twint
 import os
 
 def ScrapeHashtagTwint(hashtag, datetime):
     os.system('twint -s '+ hashtag +' --since "' + datetime +'"  -o '+ hashtag +'tweets.csv --csv ')
     pass
-
 
