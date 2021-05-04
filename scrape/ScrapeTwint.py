620de999b7886593f93c80a771e1fc58395a9910
João Ribeiro Medeiros <joao@MacBook-Air-de-Joao.local>
2021-05-04 16:06:06 -0300

    corrige identacao em scrapetwint.py

--- scrape/ScrapeTwint.py
+++ scrape/ScrapeTwint.py
@@ -2,6 +2,6 @@ import os
 
 
 def ScrapeHashtagTwint(hashtag, datetime):
-os.system('twint -s ' + hashtag + ' --since "' + datetime + 
-                 '"  -o ' + hashtag + 'tweets.csv --csv ')
+    os.system('twint -s ' + hashtag + ' --since "' + datetime + 
+                    '"  -o ' + hashtag + 'tweets.csv --csv ')
     pass
