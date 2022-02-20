0a118fd0def052c1a0b02b4bd552ca5f2012bea9
nahumsa <nahumsaa@gmail.com>
2022-02-20 16:26:15 -0300

    Make the app prettier

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from scrape.ScrapeTwint import ScrapeHashtagTwint
 from datetime import datetime, timedelta
 import nltk as nltk
 from nltk.probability import FreqDist
+from constants import COLUMNS_TO_REMOVE, DESCRIPTION_DICTIONARY
 
 from transformers import pipeline
 
@@ -94,46 +95,9 @@ class WriteApp:
         return filtered_wc
 
     def describe_classes(self, classified_lean_WordCloud):
-        description_dictionary = {
-            "CC": "coordinating conjunction",
-            "CD": "cardinal digit",
-            "DT": "determiner",
-            "EX": "existential there",
-            "FW": "foreign word",
-            "IN": "preposition/subordinating conjunction",
-            "JJ": "This NLTK POS Tag is an adjective (large)",
-            "JJR": "adjective, comparative (larger)",
-            "JJS": "adjective, superlative",
-            "LS": "list market",
-            "MD": "modal (could, will)",
-            "NN": "noun, singular (cat, tree)",
-            "NNS": "noun plural (desks)",
-            "NNP": "proper noun, singular (sarah)",
-            "NNPS": "proper noun, plural (indians or americans)",
-            "PDT": "predeterminer (all, both, half)",
-            "POS": "possessive ending (parent\ ‘s)",
-            "PRP": "personal pronoun (hers, herself, him,himself)",
-            "PRP$": "possessive pronoun (her, his, mine, my, our )",
-            "RB": "adverb (occasionally, swiftly)",
-            "RBR": "adverb, comparative (greater)",
-            "RBS": "adverb, superlative (biggest)",
-            "RP": "particle (about)",
-            "TO": "infinite marker (to)",
-            "UH": "interjection (goodbye)",
-            "VB": "verb (ask)",
-            "VBG": "verb gerund (judging)",
-            "VBD": "verb past tense (pleaded)",
-            "VBN": "verb past participle (reunified)",
-            "VBP": "verb, present tense not 3rd person singular(wrap)",
-            "VBZ": "verb, present tense with 3rd person singular (bases)",
-            "WDT": "wh-determiner (that, what)",
-            "WP": "wh- pronoun (who)",
-            "WRB": "wh- adverb (how)",
-        }
-
         from_word_to_description = {}
         for word_class in classified_lean_WordCloud:
-            from_word_to_description[word_class[0]] = description_dictionary[
+            from_word_to_description[word_class[0]] = DESCRIPTION_DICTIONARY[
                 word_class[1]
             ]
         return from_word_to_description
@@ -167,14 +131,8 @@ class WriteApp:
         st.write(example)
         return self
 
-    def translate_tweets(self):
-        translator = google_translator()
-        self.translations = translator.translate(self.tweets, lang_tgt="en")
-        return self
-
     def analyze_tweets(self):
         classifier = pipeline("sentiment-analysis")
-        # analysis_tot = [classifier(tweet)[0] for tweet in self.translations] # BYPASSING TRANSLATION GOOGLE_TRANS_NEW ISSUE
         analysis_tot = [classifier(tweet)[0] for tweet in self.tweets]
         labels = [analysis["label"] for analysis in analysis_tot]
         analysis_statistic = [
@@ -185,273 +143,180 @@ class WriteApp:
         return analysis_statistic, mean_score
 
 
-lingua = st.sidebar.selectbox("", ["pt", "en"])
+st.set_page_config(page_title="Tweetcraft",
+                   layout="wide")
+
+col1, col2 = st.columns([5, 1])
+
+with col2:
+    lingua = st.selectbox("Selecione a linguagem (Select a language)", ["pt", "en"])
 
 if lingua == "en":
-
-    st.title("Tweetcraft")
-
-    st.write("")
-
-    st.header("An App by @sa_nahum and @RioJanu")
-
-    st.write("")
-    st.write("")
-
-    st.write("Welcome to Tweetcraft!")
+    
+    with col1:
+        st.title("Tweetcraft")
+        st.header("An App by @sa_nahum and @RioJanu")
 
     st.write(
+        "Welcome to Tweetcraft!" + \
         "Enjoy a brief journey into the wonders of \
               Twitter scraping and processing"
     )
 
-    st.write("")
+    col_2_1, col_2_2 = st.columns(2)
 
-    st.write(
-        "First off, choose a word of your musing... \
-    an ingredient to the magik spell"
-    )
+    with col_2_1:
+        st.write(
+            "First off, choose a word of your musing... \
+        an ingredient to the magik spell"
+        )
 
-    hash = st.text_input("Scrape Twitter for your target word! ;)")
+        hash = st.text_input("Scrape Twitter for your target word! ;)")
 
-    st.write("")
+    with col_2_2:
+        st.write(
+            "Now, choose a time frame! Tweetcraft \
+                dwells on fresh data, \
+                so it will scrape twitter for the last \
+                N minutes where N is yours \
+                to input! remember a very large N \
+                will make the process slower"
+        )
 
-    st.write(
-        "Now, choose a time frame! Tweetcraft \
-            dwells on fresh data, \
-            so it will scrape twitter for the last \
-            N minutes where N is yours \
-            to input! remember a very large N \
-            will make the process slower"
-    )
-
-    timelapse = st.number_input(
-        "Twitter in the last N minutes!", value=5, max_value=30, min_value=1
-    )
+        timelapse = st.number_input(
+            "Twitter in the last N minutes!", value=5, max_value=30, min_value=1
+        )
 
     d = datetime.now() - timedelta(minutes=timelapse)
     horadia = d.strftime("%Y-%m-%d %H:%M:%S")
-    # horadia = d.strftime('%a %b %d %H:%M:%S %z %Y')
 
     if hash:
         ScrapeHashtagTwint(hash, horadia)
         text = WriteApp(hash)
-
-        columns_to_remove = [
-            "id",
-            "conversation_id",
-            "created_at",
-            # "time",
-            "timezone",
-            "user_id",
-            "username",
-            "name",
-            "place",
-            "language",
-            # "mentions",
-            "urls",
-            "photos",
-            # "replies_count",
-            # "retweets_count",
-            # "likes_count",
-            "date",
-            "hashtags",
-            "cashtags",
-            "link",
-            "retweet",
-            "quote_url",
-            "video",
-            "thumbnail",
-            "near",
-            "geo",
-            "source",
-            "user_rt_id",
-            "user_rt",
-            "retweet_id",
-            "reply_to",
-            "retweet_date",
-            "translate",
-            "trans_src",
-            "trans_dest",
-        ]
-        text = text.remove_columns(columns_to_remove)
+        text = text.remove_columns(COLUMNS_TO_REMOVE)
         text = text.select_tweets().remove_url().remove_punctuation()
         text = text.tokenize().remove_stopwords()
 
-        st.write(text.generate_wordcloud(500, 500, 535, (16, 9)))
+        st.pyplot(
+            text.generate_wordcloud(max_font_size=200,
+                                    width=700,
+                                    height=200,
+                                    figsize=(20, 14))
+            )
 
-        st.write("")
+        col_3_1, col_3_2, col_3_3 = st.columns(3)
 
-        with st.expander(
-            "Checkout one random tweet from the collection \
-                  you've just downloaded"
-        ):
-            st.write("")
-            text.showrandomtweet()
-
-        st.write("")
-
-        # text.translate_tweets()
+        with col_3_1:
+            with st.expander(
+                "Checkout one random tweet from the collection \
+                    you've just downloaded"
+            ):
+                st.write("")
+                text.showrandomtweet()
 
         stat, mean_score = text.analyze_tweets()
 
-        with st.expander("a quick dive into the Sentiment within our tweet set!"):
-            st.write("How much Positivity we found around this word?")
-            st.progress(stat[0])
-            st.write("What is the confidence score of this analysis")
-            st.progress(stat[1])
+        with col_3_2:
+            with st.expander("a quick dive into the Sentiment within our tweet set!"):
+                st.write("How much Positivity we found around this word?")
+                st.progress(stat[0])
+                st.write("What is the confidence score of this analysis")
+                st.progress(stat[1])
 
-        st.write("")
-        st.write("")
-
-        with st.expander("What does the HiveMind tell us?"):
-
-            text = text.select_tweets().remove_url().remove_punctuation()
-
-            text = text.tokenize().remove_stopwords()
-
-            text = text.generate_wordcloud_lean()
-
-            text = text.get_synthatic_classes()
-
-            prophecy = text.write_target_syntathic_structure()
-
-            st.write(prophecy)
-
-        st.write("")
-        st.write("")
+        with col_3_3:
+            with st.expander("What does the HiveMind tell us?"):
+                text = text.select_tweets().remove_url().remove_punctuation()
+                text = text.tokenize().remove_stopwords()
+                text = text.generate_wordcloud_lean()
+                text = text.get_synthatic_classes()
+                prophecy = text.write_target_syntathic_structure()
+                st.write(prophecy)
 
 elif lingua == "pt":
 
-    st.title("Tweetcraft")
-
-    st.write("")
-
-    st.header("Um App de @sa_nahum e @RioJanu")
-
-    st.write("")
-    st.write("")
-
-    st.write("Benvindo ao Tweetcraft!")
+    with col1:
+        st.title("Tweetcraft")
+        st.header("Um App de [@sa_nahum](https://twitter.com/sa_nahum) e [@RioJanu](https://twitter.com/RioJanu)")
 
     st.write(
+        "Bem-vindo ao Tweetcraft! " + \
         "Curta uma breve jornada pelas maravilhas \
             da extração e processamento de dados do Twitter"
     )
+    
+    col_2_1, col_2_2 = st.columns(2)
+    
+    with col_2_1:
+        st.write(
+            "Primeiramente, escolha uma palavra do seu interesse... \
+        um ingrediente para o feitiço"
+        )
+        st.write("")
+        hash = st.text_input("Busque o Twitter pela sua palavra alvo! ;)")
 
-    st.write("")
+    with col_2_2:
+        st.write(
+            "Agora escolha uma janela temporal! Tweetcraft \
+                trabalha com dados frescos quentinhos do forno, \
+                então ele buscará o Twitter \
+                nos N últimos minutos onde N é sua \
+                escolha! lembre-se que um N muito grande \
+                deixa o processo mais lento!"
+        )
 
-    st.write(
-        "Primeiramente, escolha uma palavra do seu interesse... \
-    um ingrediente para o feitiço"
-    )
-
-    hash = st.text_input("Busque o Twitter pela sua palavra alvo! ;)")
-
-    st.write("")
-
-    st.write(
-        "Agora escolha uma janela temporal! Tweetcraft \
-            trabalha com dados frescos quentinhos do forno, \
-            então ele buscará o Twitter \
-            nos N últimos minutos onde N é sua \
-            escolha! lembre-se que um N muito grande \
-            deixa o processo mais lento"
-    )
-
-    timelapse = st.number_input(
-        "Twitter nos últimos N minutos!", value=5, max_value=30, min_value=1
-    )
+        timelapse = st.number_input(
+            "Twitter nos últimos N minutos!", value=5, max_value=30, min_value=1
+        )
 
     d = datetime.today() - timedelta(minutes=timelapse)
     horadia = d.strftime("%Y-%m-%d %H:%M:%S")
-    # horadia = d.strftime('%a %b %d %H:%M:%S %z %Y')
 
     if hash:
         ScrapeHashtagTwint(hash, horadia)
         text = WriteApp(hash)
-        columns_to_remove = [
-            "id",
-            "conversation_id",
-            "created_at",
-            # "time",
-            "timezone",
-            "user_id",
-            "username",
-            "name",
-            "place",
-            "language",
-            # "mentions",
-            "urls",
-            "photos",
-            # "replies_count",
-            # "retweets_count",
-            # "likes_count",
-            "date",
-            "hashtags",
-            "cashtags",
-            "link",
-            "retweet",
-            "quote_url",
-            "video",
-            "thumbnail",
-            "near",
-            "geo",
-            "source",
-            "user_rt_id",
-            "user_rt",
-            "retweet_id",
-            "reply_to",
-            "retweet_date",
-            "translate",
-            "trans_src",
-            "trans_dest",
-        ]
-        text = text.remove_columns(columns_to_remove)
+        text = text.remove_columns(COLUMNS_TO_REMOVE)
         text = text.select_tweets().remove_url().remove_punctuation()
         text = text.tokenize().remove_stopwords()
 
-        st.write(text.generate_wordcloud(500, 500, 535, (16, 9)))
+        st.pyplot(
+            text.generate_wordcloud(max_font_size=200,
+                                    width=700,
+                                    height=200,
+                                    figsize=(20, 14))
+            )
 
-        st.write("")
+        col_3_1, col_3_2, col_3_3 = st.columns(3)
+        
+        with col_3_1:        
+            with st.expander("Dá uma olhada em um tweet aleatório do conjunto baixado"):
+                st.write("")
+                text.showrandomtweet()
 
-        with st.expander("Dá uma olhada em um tweet aleatório do conjunto baixado"):
-            st.write("")
-            text.showrandomtweet()
 
-        st.write("")
-
-        # text.translate_tweets()
 
         stat, mean_score = text.analyze_tweets()
+        with col_3_2:
+            with st.expander(
+                "Um breve mergulho no Sentimento presente no nosso conjunto de tweets!"
+            ):
+                st.write("Quanta Positividade encontramos ao redor do termo alvo?")
+                st.progress(stat[0])
+                st.write("Qual é a confiabilidade dessa análise?")
+                st.progress(stat[1])
 
-        with st.expander(
-            "Um breve mergulho no Sentimento presente no nosso conjunto de tweets!"
-        ):
-            st.write("Quanta Positividade encontramos ao redor do termo alvo?")
-            st.progress(stat[0])
-            st.write("Qual é a confiabilidade dessa análise?")
-            st.progress(stat[1])
+        with col_3_3:        
+            with st.expander("O que nos diz a Mente Geral?"):
 
-        st.write("")
-        st.write("")
+                text = text.select_tweets().remove_url().remove_punctuation()
 
-        with st.expander("O que nos diz a Mente Geral?"):
+                text = text.tokenize().remove_stopwords()
 
-            text = text.select_tweets().remove_url().remove_punctuation()
+                text = text.generate_wordcloud_lean()
 
-            text = text.tokenize().remove_stopwords()
+                text = text.get_synthatic_classes()
 
-            text = text.generate_wordcloud_lean()
+                prophecy = text.write_target_syntathic_structure()
 
-            text = text.get_synthatic_classes()
-
-            prophecy = text.write_target_syntathic_structure()
-
-            st.write(prophecy)
-
-        st.write("")
-        st.write("")
-
+                st.write(prophecy)
 
 st.button("Re-run")
